8103ca89a65e4c886e612743a653b533357b1e0f
Razix <uscreamrazix@gmail.com>
2013-10-29 13:06:02 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StripeApp::Application.config.secret_token = '20359c84b2ffa92a8bec584120e193179e236b7f455b5d599629a6ade9d65e87da74f6caac94b1c5212bd1e037f613f52dd211f29cf985904b7bc378802c2d42'
