09220f83b28a44a0ddfa8a2146d1c2ce42e07167
Razix <uscreamrazix@gmail.com>
2013-11-01 15:25:50 +0200

    add coupon discount

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :stripe_card_token, :plan_id
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :stripe_card_token, :plan_id, :coupon
   # attr_accessible :title, :body
-  attr_accessor :stripe_card_token
+  attr_accessor :stripe_card_token, :coupon
   before_destroy :terminate_subscription
 
   def save_with_payment
     if valid?
-      customer = Stripe::Customer.create(description: email, email: email, plan: plan_id, card: stripe_card_token)
+      if coupon.present?
+        customer = Stripe::Customer.create(description: email, email: email, plan: plan_id, card: stripe_card_token, coupon: coupon)
+      else
+        customer = Stripe::Customer.create(description: email, email: email, plan: plan_id, card: stripe_card_token)
+      end
       self.stripe_customer_token = customer.id
       save!
     end
@@ -23,6 +27,6 @@ class User < ActiveRecord::Base
     customer.cancel_subscription if customer.subscription.status == 'active'
   end
   rescue Stripe::InvalidRequestError => e
-  logger.error "Stripe error while creating customer: #{e.mesage}"
-  errors.add :base, "There was a problem with your credit card."
+    logger.error "Stripe error while creating customer: #{e.mesage}"
+    errors.add :base, "There was a problem with your credit card."
 end
